1f29848f7f04c3084219c5866dfd082f65ed49db
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-04-23 22:25:54 +0600

    Initial commit

---
+++ SSLCommerzIntegration/SSLCommerzIntegration/Default.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections.Specialized;
+using SSLCommerzIntegration.Models;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string baseUrl = Request.Url.Scheme + "://" + Request.Url.Authority + Request.ApplicationPath.TrimEnd('/') + "/";
+
+        // CREATING LIST OF POST DATA
+        NameValueCollection PostData = new NameValueCollection();
+        PostData.Add("total_amount", "1150.00");
+        PostData.Add("tran_id", "TESTASPNET1234");
+        PostData.Add("success_url",baseUrl+"Success.aspx");
+        PostData.Add("fail_url", baseUrl+"Fail.aspx"); // "Fail.aspx" page needs to be created
+        PostData.Add("cancel_url", baseUrl+"Cancel.aspx"); // "Cancel.aspx" page needs to be created
+        PostData.Add("version", "3.00");
+        PostData.Add("cus_name", "ABC XY");
+        PostData.Add("cus_email", "abc.xyz@mail.co");
+        PostData.Add( "cus_add1", "Address Line On");
+        PostData.Add("cus_add2", "Address Line Tw");
+        PostData.Add("cus_city", "City Nam");
+        PostData.Add("cus_state", "State Nam");
+        PostData.Add("cus_postcode", "Post Cod");
+        PostData.Add("cus_country", "Countr");
+        PostData.Add("cus_phone", "0111111111");
+        PostData.Add("cus_fax", "0171111111");
+        PostData.Add("ship_name", "testnerdcyfm3");
+        PostData.Add("ship_add1", "Address Line On");
+        PostData.Add("ship_add2", "Address Line Tw");
+        PostData.Add("ship_city", "City Nam");
+        PostData.Add("ship_state", "State Nam");
+        PostData.Add("ship_postcode", "Post Cod");
+        PostData.Add("ship_country", "Countr");
+        PostData.Add("value_a", "ref00");
+        PostData.Add("value_b", "ref00");
+        PostData.Add("value_c", "ref00");
+        PostData.Add("value_d", "ref00");
+
+        SSLCommerz sslcz = new SSLCommerz("nerdc5e82f0d18a1fc", "nerdc5e82f0d18a1fc@ssl");
+        String response = sslcz.InitiateTransaction(PostData);
+        Response.Redirect(response);
+    }
+}
---
+++ SSLCommerzIntegration/SSLCommerzIntegration/Success.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SSLCommerzIntegration.Models;
+
+public partial class Success : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!String.IsNullOrEmpty(Request.Form["status"]) && Request.Form["status"] == "VALID")
+        {
+            string TrxID = Request.Form["tran_id"];
+            // AMOUNT and Currency FROM DB FOR THIS TRANSACTION
+            string amount = "1150";
+            string currency = "BDT";
+
+            SSLCommerz sslcz = new SSLCommerz("nerdc5e82f0d18a1fc", "nerdc5e82f0d18a1fc@ssl", true);
+            Response.Write(sslcz.OrderValidate(TrxID, amount, currency, Request));
+        }
+        else {
+            Response.Write("not found");
+        }
+    }
+}
